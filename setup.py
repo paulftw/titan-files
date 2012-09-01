d1cb246e27b0a053e6ee7006041f846cbb8a0e2a
Paul Korzhyk <paul.korzhyk@gmail.com>
2012-09-01 18:01:53 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Titan Files
+-----------
+
+Titan Files is a filesystem abstraction for App Engine apps, providing a
+file-like API which uses the App Engine datastore and blobstore underneath.
+
+Links
+`````
+
+* `documentation <http://code.google.com/p/titan-files/>`_
+"""
+from setuptools import setup
+
+
+setup(
+    name='Titan-Files',
+    version='1.0',
+    url='http://code.google.com/p/titan-files/',
+    license='Apache License 2.0',
+    author='Mike Fotinakis',
+    author_email='fotinakis@google.com',
+    description='Filesystem abstraction for App Engine apps',
+    packages=['titan'],
+    namespace_packages=['titan'],
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
